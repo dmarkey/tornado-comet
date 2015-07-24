3e6b31567e4f871c3b93490d45147fc545776783
dmarkey <david@dmarkey.com>
2015-07-24 12:13:25 +0100

    improvments

--- diag_example.py
+++ diag_example.py
@@ -1,6 +1,9 @@
+from threading import Timer
 import time
 from talk_back import IncomingProcessor
 
+
+requests = []
 
 class DiagSDKProcessor(IncomingProcessor):
     service_name = "diag_sdk"
@@ -8,18 +11,33 @@ class DiagSDKProcessor(IncomingProcessor):
     def work(self, item):
 
         request_serialized = item.serialize()
-        #Emulate serialising the request to database etc
-        #time.sleep(10)
-        #Emulate some diag activity...
 
-        new_request = self.deserialize(request_serialized) # this can happen at any time on another process.
-        results = {"Results": [{"Severity": "Low"}, {"Severity": "High"}, {"Severity": "Medium"}],
-                   "original_request": item.request_data}  # included for illustration
-        new_request.finish(results)
+        requests.append(request_serialized)
+
 
     def on_failure(self, message):
         print "Message failed to be delivered"
 
+
+
+def emulate_push():
+    print "Doing"
+
+    global requests
+
+    for req in requests:
+
+        item = DiagSDKProcessor().deserialize(req) # this can happen at any time on another process.
+        results = {"Results": [{"Severity": "Low"}, {"Severity": "High"}, {"Severity": "Medium"}],
+                   "original_request": item.request_data}  # included for illustration
+        item.finish(results)
+    requests = []
+
+    Timer(30.0, emulate_push).start()
+
+
 if __name__ == "__main__":
+
+    Timer(30.0, emulate_push).start()
 
     DiagSDKProcessor().run()
